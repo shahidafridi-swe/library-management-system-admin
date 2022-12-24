d6758ff3a39ab94042a51d41b01336ed36c2bd9b
tonni-akter-dev <tonniakterbithi@gmail.com>
2022-12-24 22:26:04 +0600

    viewBooks page search section done

--- src/pages/ViewItems/ViewBooks/AllBooks.js
+++ src/pages/ViewItems/ViewBooks/AllBooks.js
@@ -43,7 +43,6 @@ const AllBooks = () => {
                                         <Link to={`/adminList`}
                                             className=''
                                         >Issue</Link>
-
                                     </td>
                                 </tr>
                             )
